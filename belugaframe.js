413ff2f190b6bfb20b9dbe88a900a548776d60c9
André Maurilio <117940329+AndreMaurilioDEV@users.noreply.github.com>
2023-03-09 16:58:07 -0300

    last up challenge

---
+++ belugaframe.js
@@ -0,0 +1,20 @@
+
+
+
+function calcularmedia(){
+
+    console.log(arguments); 
+    qtd = arguments.length;
+    var soma = 0;
+    var x = 0;
+
+   while(typeof arguments[x] === 'number'){
+    soma += arguments[x]
+    x++;
+
+   }
+
+    return soma / qtd; 
+    
+    }
+
